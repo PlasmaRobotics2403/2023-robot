50dfee0eb9bff651b923ec8d5e221bcfaee0a751
Ethan Pope <ethan@diavida.com>
2023-01-25 18:45:10 -0700

    characterization

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,22 +69,22 @@ public final class Constants {
         public static final double angleKF = chosenModule.angleKF;
 
         /* Drive Motor PID Values */
-        public static final double driveKP = 0.05; //TODO: This must be tuned to specific robot
+        public static final double driveKP = 0.13; //TODO: This must be tuned to specific robot
         public static final double driveKI = 0.0;
         public static final double driveKD = 0.0;
         public static final double driveKF = 0.0;
 
         /* Drive Motor Characterization Values 
          * Divide SYSID values by 12 to convert from volts to percent output for CTRE */
-        public static final double driveKS = (0.06 / 12); //TODO: This must be tuned to specific robot
-        public static final double driveKV = (0.35 / 12);
-        public static final double driveKA = (0.04 / 12);
+        public static final double driveKS = (0.116 / 12); //TODO: This must be tuned to specific robot
+        public static final double driveKV = (0.731 / 12);
+        public static final double driveKA = (0.091 / 12);
 
         /* Swerve Profiling Values */
         /** Meters per Second */
-        public static final double maxSpeed = 0.05; //TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 10; //TODO: This must be tuned to specific robot
         /** Radians per Second */
-        public static final double maxAngularVelocity = 0.5; //TODO: This must be tuned to specific robot
+        public static final double maxAngularVelocity = 5; //TODO: This must be tuned to specific robot
 
         /* Neutral Modes */
         public static final NeutralMode angleNeutralMode = NeutralMode.Coast;
